73c93ef09d6dcbc148193b467e3c9ad8435a8b6f
puan0601 <anton.pugach@gmail.com>
2017-06-28 16:22:23 -0700

    index.js renders hi

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 
-// Create a new component. This comonent should produce some HTML
+// Create a new component. This component should produce some HTML
 const App = function() {
     return <div>Hi!</div>;
 };
 
 // Take this componet's generate HTML and put it on the page (in the DOM)
-React.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
