c926bde8359608e5f6d73962e0350127859a14fa
puan0601 <anton.pugach@gmail.com>
2017-06-28 20:41:41 -0700

    video list populates and preview updates on click

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCnKqAcXUI_nBBG4uiDxh8CFVZ7Ah7BQ30';
 
@@ -15,10 +16,16 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({key: API_KEY, term: 'drones'}, (videos) => {
-            this.setState({ videos });
+            this.setState({ 
+                videos:videos,
+                selectedVideo: videos[0] 
+            });
         });
     }
 
@@ -26,10 +33,13 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+                videos={this.state.videos} />
             </div>
         );
-    };
-};
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
