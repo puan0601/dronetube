3be7b7c77b6a0cdfb81b9626a01bae8d1e7503e5
puan0601 <anton.pugach@gmail.com>
2017-06-28 17:23:42 -0700

    renders input on index for searchbar

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ const API_KEY = 'AIzaSyCnKqAcXUI_nBBG4uiDxh8CFVZ7Ah7BQ30';
 
 // Create a new component. This component should produce some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 };
 
 // Take this componet's generate HTML and put it on the page (in the DOM)
