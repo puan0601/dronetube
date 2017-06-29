513d2dc1e7401e185995a85bca38a9d88f29cb41
puan0601 <anton.pugach@gmail.com>
2017-06-28 18:25:05 -0700

    adds default videos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -10,14 +10,24 @@ YTSearch({key: API_KEY, term: 'drones'}, function(data) {
     console.log(data);
 });
 
-// Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'drones'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+    
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    };
 };
 
-// Take this componet's generate HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
